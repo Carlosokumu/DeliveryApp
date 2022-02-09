57da31802f4c4d643c3977d6f8ace65735f98358
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2022-02-09 15:18:32 +0300

    Update

--- app/src/main/java/com/example/deliveryapp/fragments/PaymentFragment.kt
+++ app/src/main/java/com/example/deliveryapp/fragments/PaymentFragment.kt
@@ -1,6 +1,7 @@
 package com.example.deliveryapp.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,11 +9,19 @@ import android.widget.Button
 import android.widget.TextView
 import com.example.deliveryapp.OrderInfo
 import com.example.deliveryapp.R
+import com.example.deliveryapp.models.UserInfo
 import com.example.deliveryapp.utils.ObjectBox
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.gson.Gson
+import io.socket.client.IO
+import io.socket.client.Socket
+import io.socket.emitter.Emitter
+import java.net.URISyntaxException
 
 class PaymentFragment: BottomSheetDialogFragment() {
 
+    lateinit var mSocket: Socket
+    val gson: Gson = Gson()
 
    companion object{
        lateinit var orderInfo: OrderInfo
@@ -21,6 +30,19 @@ class PaymentFragment: BottomSheetDialogFragment() {
            return PaymentFragment()
        }
    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        try {
+            mSocket = IO.socket("https://chatsocketcarlos.herokuapp.com")
+
+        } catch (e: URISyntaxException) {
+
+        }
+        mSocket.connect()
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,9 +52,25 @@ class PaymentFragment: BottomSheetDialogFragment() {
        view.findViewById<TextView>(R.id.type).text = orderInfo.name
         view.findViewById<TextView>(R.id.amount).text = orderInfo.price
 
-        view.findViewById<Button>(R.id.btnplaceOder).setOnClickListener {
+
+
+
+
+
+        var onOrder = Emitter.Listener {
+            val userInfo= gson.fromJson(it[0].toString(),UserInfo::class.java)
+             Log.d("Data",userInfo.name)
+        }
+
+        mSocket.on("newOrder",onOrder)
+
+        view.findViewById<TextView>(R.id.btnplaceOder).setOnClickListener {
             val userBox = ObjectBox.store.boxFor(OrderInfo::class.java)
             userBox.put(orderInfo)
+            val data =  UserInfo(orderInfo.name,orderInfo.name,"me")
+            val jsonData = gson.toJson(data)
+            mSocket.emit("onOrder", jsonData)
+
         }
         return view
     }
